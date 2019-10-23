41b399c0f4839259f2c4b4fdf6dce3ae6b2964fb
Aidan <9996184@student.swin.edu.au>
2019-10-23 16:01:20 +1100

    Added submit post functionality and express JSON interpreter

--- server.js
+++ server.js
@@ -23,12 +23,35 @@ catch (error) {
 }
 
 app.use('/', express.static(__dirname))
+app.use(express.json())
 
 app.get('/', function (req, res) {
   res.render('index')
 })
 app.get('/*', function (req, res) {
   res.sendFile(__dirname)
+})
+
+app.post('/submit', function (request, response) {
+  console.log('Submit request received', request.body)
+
+  try {
+    db.collection('forms').insertOne(request.body,
+      function (error, result) {
+        if (error) {
+          console.error('Submission invalid')
+          response.status(400).send(error)
+        }
+        else {
+          console.log('Database updated')
+          response.status(200).send()
+        }
+      })
+  }
+  catch (error) {
+    console.error(error)
+    response.status(500).send()
+  }
 })
 
 app.listen(8081, function () {
